4b9018d7988dc062d2218e50e7fb80efc6bc9661
robinlyang <robiny@gmail.com>
2016-06-15 09:24:16 -0600

    Project Creation
    Project Creation

---
+++ src/userInterface/UserInterface.java
@@ -0,0 +1,55 @@
+package userInterface;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.JTabbedPane;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+
+public class UserInterface extends JFrame {
+	public UserInterface() {
+		setSize(new Dimension(800, 450));
+		getContentPane().setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 58, 764, 342);
+		getContentPane().add(tabbedPane);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("New tab", null, tabbedPane_1, null);
+		
+		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("New tab", null, tabbedPane_2, null);
+		
+		JPanel panel = new JPanel();
+		tabbedPane_2.addTab("New tab", null, panel, null);
+		getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{tabbedPane}));
+		
+
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					UserInterface frame = new UserInterface();
+					frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
