d4cad1569f11892bce2b1a4873c1cca378659aa8
robinlyang <robiny@gmail.com>
2016-06-17 11:39:17 -0600

    CustomerBroker
    Created CustomerBroker RAF class and refactored Customer class
    accordingly.

--- src/controller/CustomerBroker.java
+++ src/controller/CustomerBroker.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import model.Customer;
 
 public class CustomerBroker extends RandomAccessFile
 {
@@ -16,12 +23,24 @@ public class CustomerBroker extends RandomAccessFile
 	private long highId;
 	
 	//Constructors
+	/**
+	 * Constructor is called only when the getBroker method is called.
+	 * @throws FileNotFoundException - Throw when the RandomAccessFile 
+	 * is not found on the path.
+	 */
 	public CustomerBroker() throws FileNotFoundException 
 	{
 		super(RANDOM_FILE, MODE);
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Method controls generation of CustomerBroker reference by 
+	 * checking to see if the object already exists in memory.  Part 
+	 * of the Singleton Design pattern.
+	 * @return A reference to the current existing broker or a newly 
+	 * created broker of type CustomerBroker.
+	 */
 	public static CustomerBroker getBroker()
 	{
 		File file = new File("res/clients.bin");
@@ -55,15 +74,541 @@ public class CustomerBroker extends RandomAccessFile
 	}
 
 	//Operational Methods
+	/**
+	 * Method to search the Customer RandomAccessFile back end allowing a 
+	 * search to be carried out based on customer attributes.
+	 * @param o - an Object of type Customer containing the search value of
+	 * the item to search for.
+	 * @return a List of Customer objects matching the search criteria that 
+	 * are passed on the GUI front end.  The List is originally created by 
+	 * the back end as an ArrayList and returned as a List.
+	 */
 	public List search(Object o)
 	{
-		return null;
-		
+		Customer customer = (Customer)o;
+		ArrayList list = new ArrayList();
+		if(customer.isStatus())
+		{
+			return searchStatus(list, customer.isStatus());
+		}
+		if(customer.getCustomer() != null)
+		{
+			return searchCustomer(list, customer.getCustomer());
+		}
+		if(customer.getCurrency() != null)
+		{
+			return searchCurrency(list, customer.getCurrency());
+		}
+		if((Double)customer.getBalance() != null)
+		{
+			return searchBalance(list, customer.getBalance());
+		}
+		if((Double)customer.getTotalBalance() != null)
+		{
+			return searchTotalBalance(list, customer.getTotalBalance());
+		}
+		if(customer.getCompany() != null)
+		{
+			return searchCompany(list, customer.getCompany());
+		}
+		if(customer.getTitle() != null)
+		{
+			return searchTitle(list, customer.getTitle());
+		}
+		if(customer.getFirstName() != null)
+		{
+			return searchFirstName(list, customer.getFirstName());
+		}
+		if(customer.getMiddleInitial() != null)
+		{
+			return searchMiddleInitial(list, customer.getMiddleInitial());
+		}
+		if(customer.getLastName() != null)
+		{
+			return searchLastName(list, customer.getLastName());
+		}
+		if(customer.getPrimaryContact() != null)
+		{
+			return searchPrimaryContact(list, customer.getPrimaryContact());
+		}
+		if(customer.getMainPhone() != null)
+		{
+			return searchMainPhone(list, customer.getMainPhone());
+		}
+		if(customer.getFax() != null)
+		{
+			return searchFax(list, customer.getFax());
+		}
+		if(customer.getAlternativePhone() != null)
+		{
+			return searchAlternativePhone(list, customer.getAlternativePhone());
+		}
+		if(customer.getSecondaryContact() != null)
+		{
+			return searchSecondaryContact(list, customer.getSecondaryContact());
+		}
+		if(customer.getJobTitle() != null)
+		{
+			return searchJobTitle(list, customer.getJobTitle());
+		}
+		if(customer.getMainEmail() != null)
+		{
+			return searchMainEmail(list, customer.getMainEmail());
+		}
+		if(customer.getInvoiceToOne() != null)
+		{
+			return searchInvoiceToOne(list, customer.getInvoiceToOne());
+		}
+		if(customer.getInvoiceToTwo() != null)
+		{
+			return searchInvoiceToTwo(list, customer.getInvoiceToTwo());
+		}
+		if(customer.getInvoiceToThree() != null)
+		{
+			return searchInvoiceToThree(list, customer.getInvoiceToThree());
+		}
+		if(customer.getInvoiceToFour() != null)
+		{
+			return searchInvoiceToFour(list, customer.getInvoiceToFour());
+		}
+		if(customer.getInvoiceToFive() != null)
+		{
+			return searchInvoiceToFive(list, customer.getInvoiceToFive());
+		}
+		if(customer.getShipToOne() != null)
+		{
+			return searchShipToOne(list, customer.getShipToOne());
+		}
+		if(customer.getShipToTwo() != null)
+		{
+			return searchShipToTwo(list, customer.getShipToTwo());
+		}
+		if(customer.getShipToThree() != null)
+		{
+			return searchShipToThree(list, customer.getShipToThree());
+		}
+		if(customer.getShipToFour() != null)
+		{
+			return searchShipToFour(list, customer.getShipToFour());
+		}
+		if(customer.getShipToFive() != null)
+		{
+			return searchShipToFive(list, customer.getShipToFive());
+		}
+		if(customer.getCustomerType() != null)
+		{
+			return searchCustomerType(list, customer.getCustomerType());
+		}
+		if(customer.getTerms() != null)
+		{
+			return searchTerms(list, customer.getTerms());
+		}
+		if(customer.getRep() != null)
+		{
+			return searchRep(list, customer.getRep());
+		}
+		if(customer.getSalesTaxCode() != null)
+		{
+			return searchSalesTaxCode(list, customer.getSalesTaxCode());
+		}
+		if(customer.getTaxCountry() != null)
+		{
+			return searchTaxCountry(list, customer.getTaxCountry());
+		}
+		if(customer.getBusinessNumber() != null)
+		{
+			return searchBusinessNumber(list, customer.getBusinessNumber());
+		}
+		if(customer.getAccountNumber() != null)
+		{
+			return searchAccountNumber(list, customer.getAccountNumber());
+		}
+		if(customer.getCreditLimit() != null)
+		{
+			return searchCreditLimit(list, customer.getCreditLimit());
+		}
+		if(customer.getJobStatus() != null)
+		{
+			return searchJobStatus(list, customer.getJobStatus());
+		}
+		if(customer.getJobType() != null)
+		{
+			return searchJobType(list, customer.getJobType());
+		}
+		if(customer.getJobDescription() != null)
+		{
+			return searchJobDescription(list, customer.getJobDescription());
+		}
+		if(customer.getStartDate() != null)
+		{
+			return searchStartDate(list, customer.getStartDate());
+		}
+		if(customer.getProjectedEnd() != null)
+		{
+			return searchProjectEnd(list, customer.getProjectedEnd());
+		}
+		if(customer.getEndDate() != null)
+		{
+			return searchEndDate(list, customer.getEndDate());
+		}
+				
+		return list;
 	}
 	
-	private void loadCustomerBinaryFile() {
+	/**
+	 * Method to add a new or modify a record to the customers binary file.
+	 * @param o - an Object of type Customer containing all information about 
+	 * a customer to be written to the customer's binary file called customer.bin.
+	 * @return a boolean confirming that the customer record has been successfully 
+	 * added or modified.
+	 */
+	public boolean persist(Object o)
+	{
+		Customer c = (Customer)o;
+		try
+		{
+			if(c.getCustomerID() == 0L)
+			{
+				c.setCustomerID(++this.highId);
+				seek(length());
+				writeRecord(c);
+				return true;
+			}
+			long byteOffset = findCustomerID(c.getCustomerID());
+			if (byteOffset >= 0L)
+			{
+				seek(byteOffset);
+				writeRecord(c);
+				return true;
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Method to logically delete a record from the client binary file.
+	 * @param o - an Object of type Customer containing all information about 
+	 * a customer to be deleted from the customer binary file called customer.bin.
+	 * @return a boolean confirming that the customer record has been successfully 
+	 * deleted.
+	 */
+	public boolean remove(Object o)
+	{
+		Customer c = (Customer)o;
+		try
+		{
+			long byteOffset = findCustomerID(c.getCustomerID());
+			if(byteOffset > 0L)
+			{
+				seek(byteOffset);
+				writeBoolean(false);
+				return true;
+			}
+			return false;
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	/**
+	 * Method to release resources allocated to the broker and save all modified data. 
+	 * NOTE if this method is called data may be lost.
+	 */
+	public void closeBroker()
+	{
+		try
+		{
+			customerBroker.close();
+			customerBroker = null;
+		}
+		catch (IOException e)
+		{
+			System.out.println("Failed to close CustomerBroker properly");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Creates a RandomAccessFile as a data structure to store customer information 
+	 * in a binary format.  This method is used only to create a database file if 
+	 * no binary file for the customer's currently exists.
+	 */
+	private void loadCustomerBinaryFile() 
+	{
+		try
+		{
+			BufferedReader fin = new BufferedReader(new FileReader("res/customers.txt"));
+			long customerID = 1L;
+			String line = fin.readLine();
+			while (line != null)
+			{
+				line = customerID++ + ";" + line;
+				Customer c = new Customer(line);
+				writeRecord(c);
+				line = fin.readLine();
+			}
+			fin.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * A method to determine the byte offset of a client record stored in the 
+	 * random access file.
+	 * @param customerID - the ID of the customer record being located.
+	 * @return the byte offset from the beginning of the client file.
+	 */
+	private long findCustomerID(long customerID) 
+	{
+		try
+		{
+			seek(0L);
+			for (long i = 0L; i < length(); i += 891)
+			{
+				seek(i);
+				Customer c = readRecord();
+				if((c.getCustomerID() == customerID) && (c.isStatus()))
+				{
+					seek(i);
+					return getFilePointer();
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return -1L;
+	}
+
+	private Customer readRecord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void writeRecord(Customer c) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	private List searchEndDate(ArrayList list, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchProjectEnd(ArrayList list, Date projectedEnd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchStartDate(ArrayList list, Date startDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchJobDescription(ArrayList list, String jobDescription) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchJobType(ArrayList list, String jobType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchJobStatus(ArrayList list, String jobStatus) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchCreditLimit(ArrayList list, String creditLimit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchAccountNumber(ArrayList list, String accountNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchBusinessNumber(ArrayList list, String businessNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchTaxCountry(ArrayList list, String taxCountry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchSalesTaxCode(ArrayList list, String salesTaxCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchRep(ArrayList list, String rep) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchTerms(ArrayList list, String terms) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchCustomerType(ArrayList list, String customerType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchShipToFive(ArrayList list, String shipToFive) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchShipToFour(ArrayList list, String shipToFour) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchShipToThree(ArrayList list, String shipToThree) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchShipToTwo(ArrayList list, String shipToTwo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchShipToOne(ArrayList list, String shipToOne) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchInvoiceToFive(ArrayList list, String invoiceToFive) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchInvoiceToFour(ArrayList list, String invoiceToFour) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchInvoiceToThree(ArrayList list, String invoiceToThree) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchInvoiceToTwo(ArrayList list, String invoiceToTwo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchInvoiceToOne(ArrayList list, String invoiceToOne) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchMainEmail(ArrayList list, String mainEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchJobTitle(ArrayList list, String jobTitle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchSecondaryContact(ArrayList list, String secondaryContact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchAlternativePhone(ArrayList list, String alternativePhone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchFax(ArrayList list, String fax) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchMainPhone(ArrayList list, String mainPhone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchPrimaryContact(ArrayList list, String primaryContact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchMiddleInitial(ArrayList list, String middleInitial) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchLastName(ArrayList list, String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchFirstName(ArrayList list, String firstName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchTitle(ArrayList list, String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchCompany(ArrayList list, String company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchTotalBalance(ArrayList list, double totalBalance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchBalance(ArrayList list, double balance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchCurrency(ArrayList list, String currency) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchCustomer(ArrayList list, String customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List searchStatus(ArrayList list, boolean status) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	private void findHighestCurrentId() {
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,58 +1,172 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.StringTokenizer;
 
 public class Customer {
 	//Attributes
-	private boolean status; 			// 1 byte
-	private String customer;			// 22 bytes
-	private String currency;			// 22 bytes
-	private double balance;				// 8 bytes
-	private double totalBalance;		// 8 bytes
-	private String company;				// 22 bytes
-	private String title;				// 22 bytes			//Mr. Mrs. Miss
-	private String firstName;			// 22 bytes	
-	private String middleInitial;		// 22 bytes	
-	private String lastName;			// 22 bytes	
-	private String primaryContact;		// 22 bytes	
-	private String mainPhone;			// 22 bytes	
-	private String fax;					// 22 bytes	
-	private String alternativePhone;	// 22 bytes	
-	private String secondaryContact;	// 22 bytes	
-	private String jobTitle;			// 22 bytes	
-	private String mainEmail;			// 22 bytes	
-	private String invoiceToOne;		// 22 bytes	
-	private String invoiceToTwo;		// 22 bytes	
-	private String invoiceToThree;		// 22 bytes	
-	private String invoiceToFour;		// 22 bytes	
-	private String invoiceToFive;		// 22 bytes	
-	private String shipToOne;			// 22 bytes	
-	private String shipToTwo;			// 22 bytes	
-	private String shipToThree;			// 22 bytes	
-	private String shipToFour;			// 22 bytes	
-	private String shipToFive;			// 22 bytes	
-	private String customerType;		// 22 bytes	
-	private String terms;				// 22 bytes	
-	private String rep;					// 22 bytes	
-	private String salesTaxCode;		// 22 bytes	
-	private String taxCountry;			// 22 bytes	
-	private String businessNumber;		// 22 bytes	
-	private String accountNumber;		// 22 bytes	
-	private String creditLimit;			// 22 bytes	
-	private String jobStatus;			// 22 bytes	
-	private String jobType;				// 22 bytes	
-	private String jobDescription;		// 22 bytes	
-	here
-	private Date startDate;				//TODO Find byte size of Date
-	private Date projectedEnd;
-	private Date endDate;
+	private long customerID;			// 8 bytes			
+	private boolean status; 			// 1 byte			9
+	private String customer;			// 22 bytes			31
+	private String currency;			// 22 bytes			53
+	private double balance;				// 8 bytes			61
+	private double totalBalance;		// 8 bytes			69
+	private String company;				// 22 bytes			91
+	private String title;				// 22 bytes			113		//Mr. Mrs. Miss
+	private String firstName;			// 22 bytes			135
+	private String middleInitial;		// 22 bytes			157
+	private String lastName;			// 22 bytes			179
+	private String primaryContact;		// 22 bytes			201
+	private String mainPhone;			// 22 bytes			223
+	private String fax;					// 22 bytes			245
+	private String alternativePhone;	// 22 bytes			267
+	private String secondaryContact;	// 22 bytes			289
+	private String jobTitle;			// 22 bytes			311
+	private String mainEmail;			// 22 bytes			333
+	private String invoiceToOne;		// 22 bytes			355
+	private String invoiceToTwo;		// 22 bytes			377
+	private String invoiceToThree;		// 22 bytes			399
+	private String invoiceToFour;		// 22 bytes			421
+	private String invoiceToFive;		// 22 bytes			443
+	private String shipToOne;			// 22 bytes			465
+	private String shipToTwo;			// 22 bytes			487
+	private String shipToThree;			// 22 bytes			509
+	private String shipToFour;			// 22 bytes			531
+	private String shipToFive;			// 22 bytes			553
+	private String customerType;		// 22 bytes			575
+	private String terms;				// 22 bytes			597
+	private String rep;					// 22 bytes			619
+	private String salesTaxCode;		// 22 bytes			641
+	private String taxCountry;			// 22 bytes			663
+	private String businessNumber;		// 22 bytes			685
+	private String accountNumber;		// 22 bytes			707
+	private String creditLimit;			// 22 bytes			729
+	private String jobStatus;			// 22 bytes			751
+	private String jobType;				// 22 bytes			773
+	private String jobDescription;		// 22 bytes			795
+	private Date startDate;				// 32 bytes			827
+	private Date projectedEnd;			// 32 bytes			859
+	private Date endDate;				// 32 bytes			891
 	
 	//Constructors
-	public Customer() {
+	/**
+	 * Creates and instance of this object with default values.
+	 */
+	public Customer() 
+	{
 		super();
+		this.status = true;
+	}
+	
+	/**
+	 * Creates an instance of a Customer object with a specified list of initial 
+	 * values abed on the string input.
+	 * @param line - Contains a complete set of client information.
+	 */
+	public Customer(String line)
+	{
+		StringTokenizer token = new StringTokenizer(line, ";");
+		this.customerID = Long.parseLong(token.nextToken());
+		this.status = Boolean.valueOf(token.nextToken());
+		this.customer = token.nextToken();
+		this.currency = token.nextToken();
+		this.balance = Double.parseDouble(token.nextToken());
+		this.totalBalance = Double.parseDouble(token.nextToken());
+		this.company = token.nextToken();
+		this.title = token.nextToken();
+		this.firstName = token.nextToken();
+		this.middleInitial = token.nextToken();
+		this.lastName = token.nextToken();
+		this.primaryContact = token.nextToken();
+		this.mainPhone = token.nextToken();
+		this.fax = token.nextToken();
+		this.alternativePhone = token.nextToken();
+		this.secondaryContact = token.nextToken();
+		this.jobTitle  = token.nextToken();
+		this.mainEmail = token.nextToken();
+		this.invoiceToOne = token.nextToken();
+		this.invoiceToTwo = token.nextToken();
+		this.invoiceToThree = token.nextToken();
+		this.invoiceToFour = token.nextToken();
+		this.invoiceToFive = token.nextToken();
+		this.shipToOne = token.nextToken();
+		this.shipToTwo = token.nextToken();
+		this.shipToThree = token.nextToken();
+		this.shipToFour = token.nextToken();
+		this.shipToFive = token.nextToken();
+		this.customerType = token.nextToken();
+		this.terms = token.nextToken();
+		this.rep = token.nextToken();
+		this.salesTaxCode = token.nextToken();
+		this.taxCountry = token.nextToken();
+		this.businessNumber = token.nextToken();
+		this.accountNumber = token.nextToken();
+		this.creditLimit = token.nextToken();
+		this.jobStatus = token.nextToken();
+		this.jobType = token.nextToken();
+		this.jobDescription = token.nextToken();
+		SimpleDateFormat formatter = new SimpleDateFormat("MMM dd, yyyy");
+		try 
+		{
+			this.startDate = formatter.parse(token.nextToken());
+			this.projectedEnd = formatter.parse(token.nextToken());
+			this.endDate = formatter.parse(token.nextToken());
+		} 
+		catch (ParseException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
-	public Customer(boolean status, String customer, String currency, double balance, double totalBalance,
+	/**
+	 * Creates an instance of a Customer object with a specified list of initial 
+	 * values based on the string input.
+	 * @param customerID - The customer's ID number
+	 * @param status - The customer's status
+	 * @param customer - The customer
+	 * @param currency - The customer's currency
+	 * @param balance - The customer's balance
+	 * @param totalBalance - The customer's total balance
+	 * @param company - The customer's company
+	 * @param title - The customer's title
+	 * @param firstName - The customer's first name
+	 * @param middleInitial - The customer's middle initial
+	 * @param lastName - The customer's last name
+	 * @param primaryContact - The customer's primary contact
+	 * @param mainPhone - The customer's main phone number
+	 * @param fax - The customer's fax number
+	 * @param alternativePhone - The customer's alternative phone number
+	 * @param secondaryContact - The customer's secondary phone number
+	 * @param jobTitle - The customer's job title
+	 * @param mainEmail - The customer's main email
+	 * @param invoiceToOne - The customer's invoice line one
+	 * @param invoiceToTwo - The customer's invoice line two
+	 * @param invoiceToThree - The customer's invoice line three
+	 * @param invoiceToFour - The customer's invoice line four
+	 * @param invoiceToFive - The customer's invoice line five
+	 * @param shipToOne - The customer's ship line one 
+	 * @param shipToTwo - The customer's ship line two
+	 * @param shipToThree - The customer's ship line three
+	 * @param shipToFour - The customer's ship line four
+	 * @param shipToFive - The customer's ship line five
+	 * @param customerType - The customer's type
+	 * @param terms - The customer's terms
+	 * @param rep - The customer's representative
+	 * @param salesTaxCode - The customer's sales tax code
+	 * @param taxCountry - The customer's tax country
+	 * @param businessNumber - The customer's business number
+	 * @param accountNumber - The customer's account number
+	 * @param creditLimit - The customer's credit limit
+	 * @param jobStatus - The customer's job status
+	 * @param jobType - The customer's job type
+	 * @param jobDescription - The customer's job description
+	 * @param startDate - The customer's start date
+	 * @param projectedEnd - The customer's projected end date
+	 * @param endDate - The customer's end date
+	 */
+	public Customer(long customerID, boolean status, String customer, String currency, double balance, double totalBalance,
 			String company, String title, String firstName, String middleInitial, String lastName,
 			String primaryContact, String mainPhone, String fax, String alternativePhone, String secondaryContact,
 			String jobTitle, String mainEmail, String invoiceToOne, String invoiceToTwo, String invoiceToThree,
@@ -61,6 +175,7 @@ public class Customer {
 			String taxCountry, String businessNumber, String accountNumber, String creditLimit, String jobStatus,
 			String jobType, String jobDescription, Date startDate, Date projectedEnd, Date endDate) {
 		super();
+		this.customerID = customerID;
 		this.status = status;
 		this.customer = customer;
 		this.currency = currency;
@@ -350,6 +465,12 @@ public class Customer {
 	}
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
+	}
+	public long getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(long customerID) {
+		this.customerID = customerID;
 	}
 	
 	
